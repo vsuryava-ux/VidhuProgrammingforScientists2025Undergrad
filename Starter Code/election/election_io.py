c0526b852053f1eaf0b6e07a1b2a9477a0489295
Phillip Compeau <phcompeau@gmail.com>
2025-09-15 17:38:10 -0400

    Adding starter and finished code for elections

--- Starter Code/election/election_io.py
+++ Starter Code/election/election_io.py
@@ -1,1 +1,26 @@
-import csv
+def read_electoral_votes(filename: str) -> dict[str, int]:
+    """
+    Processes the number of electoral votes for each state.
+    Parameters:
+    - filename (str): A filename string.
+    Returns:
+    - dict[str, int]: A dictionary that associates each state name (string)
+      to an integer corresponding to its number of Electoral College votes.
+    """
+    if not filename or not isinstance(filename, str):
+        raise ValueError("filename must be a non-empty string.")
+
+    pass
+
+def read_polling_data(filename: str) -> dict[str, float]:
+    """
+    Parses polling percentages from a file.
+    Parameters:
+    - filename (str): A filename string.
+    Returns:
+    - dict[str, float]: A dictionary of state names (strings) to floats
+      corresponding to the percentages for candidate 1.
+    """
+
+    if not filename or not isinstance(filename, str):
+        raise ValueError("filename must be a non-empty string.")
--- Starter Code/election/main.py
+++ Starter Code/election/main.py
@@ -2,3 +2,6 @@ import random  # for generating random numbers
 
 def main() :
 	print("Let's simulate an election!")
+
+if __name__ == "__main__":
+    main()
